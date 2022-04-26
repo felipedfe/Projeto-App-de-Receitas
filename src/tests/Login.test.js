7ed99dbaac6cc83ea3c4f1d3b6815a3df9ee0b59
Débora Serra <debora.r.serra@gmail.com>
2022-04-26 16:52:56 -0300

    Add login page

---
+++ src/tests/Login.test.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './helpers/renderWithRouter';
+import App from '../App';
+
+const EMAIL = 'test@test.com';
+const PASSWORD = '1234567';
+const INVALID_EMAIL = 'test@test';
+const INVALID_PASSWORD = '123';
+
+describe('Test the Login page', () => {
+  it('tests the route of the page', () => {
+    const { history } = renderWithRouter(<App />);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+  it('tests if there is an email input', () => {
+    renderWithRouter(<App />);
+    const emailInput = screen.getAllByRole('textbox')[0];
+    expect(emailInput).toBeInTheDocument();
+    expect(emailInput).toHaveAttribute('placeholder', 'E-mail');
+    expect(emailInput).toHaveAttribute('type', 'email');
+  });
+  it('tests if there is a password input', () => {
+    renderWithRouter(<App />);
+    const passwordInput = screen.getByTestId('password-input');
+    expect(passwordInput).toBeInTheDocument();
+    expect(passwordInput).toHaveAttribute('placeholder', 'Password');
+    expect(passwordInput).toHaveAttribute('type', 'password');
+  });
+  it('tests if there is an Enter button', () => {
+    renderWithRouter(<App />);
+    const enterBtn = screen.getByRole('button');
+    expect(enterBtn).toBeInTheDocument();
+    expect(enterBtn).toBeDisabled();
+    expect(enterBtn).toHaveTextContent('Enter');
+  });
+  it('verifies if it\'s possible to write an email', () => {
+    renderWithRouter(<App />);
+    const emailInput = screen.getByRole('textbox');
+    expect(emailInput.value).toBe('');
+    userEvent.type(emailInput, EMAIL);
+    expect(emailInput.value).toBe(EMAIL);
+  });
+  it('verifies if it\'s possible to type a password', () => {
+    renderWithRouter(<App />);
+    const passwordInput = screen.getByTestId('password-input');
+    expect(passwordInput.value).toBe('');
+    userEvent.type(passwordInput, PASSWORD);
+    expect(passwordInput.value).toBe(PASSWORD);
+  });
+  it('checks if the button is disabled when the email or passwor is invalid', () => {
+    renderWithRouter(<App />);
+    const emailInput = screen.getByRole('textbox');
+    const passwordInput = screen.getByTestId('password-input');
+    const sendBtn = screen.getByRole('button');
+    userEvent.type(emailInput, INVALID_EMAIL);
+    userEvent.type(passwordInput, PASSWORD);
+    expect(sendBtn).toBeDisabled();
+    userEvent.type(emailInput, EMAIL);
+    userEvent.type(passwordInput, INVALID_PASSWORD);
+    expect(sendBtn).toBeDisabled();
+  });
+  it('checks if the user is redirected when the form is submited', async () => {
+    const { history } = renderWithRouter(<App />);
+    const emailInput = screen.getByRole('textbox');
+    const passwordInput = screen.getByTestId('password-input');
+    const sendBtn = screen.getByRole('button');
+
+    userEvent.type(emailInput, EMAIL);
+    userEvent.type(passwordInput, PASSWORD);
+    expect(sendBtn).toBeEnabled();
+    userEvent.click(sendBtn);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/foods');
+  });
+});
